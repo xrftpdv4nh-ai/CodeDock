7425f2e2bd677872d8c6735376bc9c7f46b1662c
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-13 22:50:46 +0200

    Update mongo.js

--- database/mongo.js
+++ database/mongo.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 
 module.exports = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 5000
+    });
     console.log("🟢 MongoDB Connected");
   } catch (err) {
-    console.error("🔴 MongoDB Connection Error");
+    console.error("🔴 MongoDB Connection Failed");
     console.error(err);
   }
 };
