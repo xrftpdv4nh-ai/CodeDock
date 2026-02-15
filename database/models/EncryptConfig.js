04bedf97aeaabfacea90f53bff3e8376486c4677
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-02-15 02:35:17 +0200

    Update EncryptConfig.js

--- database/models/EncryptConfig.js
+++ database/models/EncryptConfig.js
@@ -1,8 +1,18 @@
 const mongoose = require("mongoose");
 
-const encryptConfigSchema = new mongoose.Schema({
-  guildId: { type: String, unique: true },
-  channels: { type: [String], default: [] }
+const EncryptConfigSchema = new mongoose.Schema({
+  guildId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  channels: {
+    type: [String],
+    default: []
+  }
 });
 
-module.exports = mongoose.model("EncryptConfig", encryptConfigSchema);
+module.exports = mongoose.model(
+  "EncryptConfig",
+  EncryptConfigSchema
+);
